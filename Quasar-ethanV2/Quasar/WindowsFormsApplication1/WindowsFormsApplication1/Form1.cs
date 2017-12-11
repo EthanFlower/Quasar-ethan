5d084edc6e00294be679e89657bcd90fbd8a373b
EthanFlower <32441639+EthanFlower@users.noreply.github.com>
2017-12-10 21:40:23 -0500

    Add files via upload

---
+++ Quasar-ethanV2/Quasar/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        /*
+        Shortcut keys
+        Ctrl K -> Ctrl D :Format whole document  
+          
+       
+        List of stuff to do or can do
+        1.change the max bet amount to their points
+        2.hide cheat buttons/numeric up downs that appear if screen is expanded?
+             
+        */
+
+
+        private Die die = new Die();
+        private int lastNum;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void die18_Click(object sender, EventArgs e)
+        {
+            if (lastNum <= 20)
+            {
+                lastNum += die.roll(1, 8);
+                label1.Text = "" + lastNum;
+            }
+        }
+
+        private void die47_Click(object sender, EventArgs e)
+        {
+            if (lastNum <= 20)
+            {
+
+                lastNum += die.roll(4, 7);
+                label1.Text = "" + lastNum;
+            }
+        }
+    }
+}
