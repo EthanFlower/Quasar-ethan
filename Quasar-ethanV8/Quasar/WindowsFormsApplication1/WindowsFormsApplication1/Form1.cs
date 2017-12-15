ec0077ba3f8428af7bd2e5d682b1e46cce4c36a7
EthanFlower <32441639+EthanFlower@users.noreply.github.com>
2017-12-15 13:27:15 -0500

    Add files via upload

---
+++ Quasar-ethanV8/Quasar/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        /*
+        Fixed ---- LOOK AT THE newMnyValue! IT CANNOT WORK WITH 15, 16, 18, 19 DUE TO DECIMALS! FIIIIIIIXXXXXXX!!!! 
+         
+        Shortcut keys
+        Ctrl K -> Ctrl D :Format whole document  
+
+        Info
+        do - while loop - do{this code}while(this is true);
+       
+        List of stuff to do or can do
+        1.change the max bet amount to their points
+        2.hide cheat buttons/numeric up downs that appear if screen is expanded?
+        3.
+        */
+        private Betting betting;
+        private Die die = new Die();
+        private Turn turn = new Turn();
+        //private Array Strings = new Array[]{"Youwentver!", "Rolled:" }; trying to have preset strings. possibly doing in future update
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        public void die18_Click(object sender, EventArgs e)
+        {
+            die.rolled(1, 8);
+        }
+
+        public void die47_Click(object sender, EventArgs e)
+        {
+            die.rolled(4, 7);
+        }
+
+        public void button3_Click(object sender, EventArgs e)
+        {
+            //have turn change when fold && give money back
+            if (turn.foldTOrNextF == true)
+            {
+                turn.prepNextRound();
+            }//button is then used to start the new round
+            else if (turn.foldTOrNextF == false)
+            {
+                turn.nextRound();
+            }
+        }
+        public void betButt_Click(object sender, EventArgs e)
+        {
+            betting.setBet();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            betting = new Betting(this);
+
+
+        turn.newRound();
+        }
+    }
+
+}
---
+++ Quasar-ethanV8/Quasar/WindowsFormsApplication1/WindowsFormsApplication1/Turn.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Turn
+    {
+        private Betting betting = new Betting();
+        private Form1 form1 = new Form1(); 
+        private Die die = new Die();
+        public Boolean foldTOrNextF = true;//form or here?
+        public void newRound()
+        {
+            //allows for the customization of the game & used after each turn
+            int totalMoney = betting.totalMoney;
+            form1.mnyNum.Text = "" + totalMoney;
+            form1.betUpDown.Maximum = totalMoney;//max bet amount
+            form1.betUpDown.Minimum = (totalMoney / 50) * 11;//min bet amount made so you have to win to keep playing
+            form1.betUpDown.Value = form1.betUpDown.Minimum;//set starting bet to minimum
+            die.lastNum = die.roll(1, 4);
+            form1.label1.Text = "" + die.lastNum;
+            form1.rollNum.Text = "Rolled: Nothing Yet";
+            form1.wentOver.Text = "";
+            form1.profitLb.Text = "";
+            form1.die18.Enabled = false;
+            form1.die47.Enabled = false;
+            form1.button3.Enabled = false;//fold
+            form1.betUpDown.Enabled = true;
+            form1.betButt.Enabled = true;
+        }
+
+        public void prepNextRound() {
+            betting.getNewMnyValue();
+            form1.die18.Enabled = false;
+            form1.die47.Enabled = false;
+            form1.button3.Text = "Next Round";
+            foldTOrNextF = false;
+        }
+        public void nextRound() {
+            newRound();
+            form1.button3.Text = "Fold";
+            foldTOrNextF = true;
+        }
+    }
+}
