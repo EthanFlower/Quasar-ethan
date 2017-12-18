aa58572394650965592aaac2287b3cdbf5c4c6cd
EthanFlower <32441639+EthanFlower@users.noreply.github.com>
2017-12-17 22:03:01 -0500

    Add files via upload

---
+++ Quasar-ethanFinalVersion/Quasar/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.die18 = new System.Windows.Forms.Button();
+            this.die47 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.betUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mnyNum = new System.Windows.Forms.Label();
+            this.mnyLb = new System.Windows.Forms.Label();
+            this.rollNum = new System.Windows.Forms.Label();
+            this.wentOver = new System.Windows.Forms.Label();
+            this.betButt = new System.Windows.Forms.Button();
+            this.profitLb = new System.Windows.Forms.Label();
+            this.buttInstruct = new System.Windows.Forms.Button();
+            this.lbRound = new System.Windows.Forms.Label();
+            this.buttReset = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.betUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // die18
+            // 
+            this.die18.Location = new System.Drawing.Point(12, 118);
+            this.die18.Name = "die18";
+            this.die18.Size = new System.Drawing.Size(122, 44);
+            this.die18.TabIndex = 0;
+            this.die18.Text = "1-8";
+            this.die18.UseVisualStyleBackColor = true;
+            this.die18.Click += new System.EventHandler(this.die18_Click);
+            // 
+            // die47
+            // 
+            this.die47.Location = new System.Drawing.Point(277, 118);
+            this.die47.Name = "die47";
+            this.die47.Size = new System.Drawing.Size(122, 44);
+            this.die47.TabIndex = 1;
+            this.die47.Text = "4-7";
+            this.die47.UseVisualStyleBackColor = true;
+            this.die47.Click += new System.EventHandler(this.die47_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(152, 137);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 25);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Fold";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(274, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "This is the amount";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // betUpDown
+            // 
+            this.betUpDown.Location = new System.Drawing.Point(145, 65);
+            this.betUpDown.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.betUpDown.Minimum = new decimal(new int[] {
+            110,
+            0,
+            0,
+            0});
+            this.betUpDown.Name = "betUpDown";
+            this.betUpDown.Size = new System.Drawing.Size(120, 20);
+            this.betUpDown.TabIndex = 4;
+            this.betUpDown.Value = new decimal(new int[] {
+            110,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(193, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Bet";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mnyNum
+            // 
+            this.mnyNum.AutoSize = true;
+            this.mnyNum.Location = new System.Drawing.Point(12, 28);
+            this.mnyNum.Name = "mnyNum";
+            this.mnyNum.Size = new System.Drawing.Size(25, 13);
+            this.mnyNum.TabIndex = 6;
+            this.mnyNum.Text = "500";
+            this.mnyNum.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // mnyLb
+            // 
+            this.mnyLb.AutoSize = true;
+            this.mnyLb.Location = new System.Drawing.Point(12, 12);
+            this.mnyLb.Name = "mnyLb";
+            this.mnyLb.Size = new System.Drawing.Size(36, 13);
+            this.mnyLb.TabIndex = 7;
+            this.mnyLb.Text = "Points";
+            this.mnyLb.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // rollNum
+            // 
+            this.rollNum.AutoSize = true;
+            this.rollNum.Location = new System.Drawing.Point(274, 25);
+            this.rollNum.Name = "rollNum";
+            this.rollNum.Size = new System.Drawing.Size(111, 13);
+            this.rollNum.TabIndex = 8;
+            this.rollNum.Text = "This is what you rolled";
+            this.rollNum.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // wentOver
+            // 
+            this.wentOver.AutoSize = true;
+            this.wentOver.Location = new System.Drawing.Point(274, 38);
+            this.wentOver.Name = "wentOver";
+            this.wentOver.Size = new System.Drawing.Size(159, 13);
+            this.wentOver.TabIndex = 9;
+            this.wentOver.Text = "This tells you that you went over";
+            this.wentOver.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // betButt
+            // 
+            this.betButt.Location = new System.Drawing.Point(173, 91);
+            this.betButt.Name = "betButt";
+            this.betButt.Size = new System.Drawing.Size(65, 23);
+            this.betButt.TabIndex = 10;
+            this.betButt.Text = "Bet";
+            this.betButt.UseVisualStyleBackColor = true;
+            this.betButt.Click += new System.EventHandler(this.betButt_Click);
+            // 
+            // profitLb
+            // 
+            this.profitLb.AutoSize = true;
+            this.profitLb.Location = new System.Drawing.Point(12, 41);
+            this.profitLb.Name = "profitLb";
+            this.profitLb.Size = new System.Drawing.Size(106, 13);
+            this.profitLb.TabIndex = 11;
+            this.profitLb.Text = "This is what you won";
+            this.profitLb.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // buttInstruct
+            // 
+            this.buttInstruct.Location = new System.Drawing.Point(152, 13);
+            this.buttInstruct.Name = "buttInstruct";
+            this.buttInstruct.Size = new System.Drawing.Size(107, 25);
+            this.buttInstruct.TabIndex = 12;
+            this.buttInstruct.Text = "Instructions";
+            this.buttInstruct.UseVisualStyleBackColor = true;
+            this.buttInstruct.Click += new System.EventHandler(this.buttInstruct_Click);
+            // 
+            // lbRound
+            // 
+            this.lbRound.AutoSize = true;
+            this.lbRound.Location = new System.Drawing.Point(274, 72);
+            this.lbRound.Name = "lbRound";
+            this.lbRound.Size = new System.Drawing.Size(85, 13);
+            this.lbRound.TabIndex = 13;
+            this.lbRound.Text = "This is the round";
+            this.lbRound.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // buttReset
+            // 
+            this.buttReset.Location = new System.Drawing.Point(277, 88);
+            this.buttReset.Name = "buttReset";
+            this.buttReset.Size = new System.Drawing.Size(107, 25);
+            this.buttReset.TabIndex = 14;
+            this.buttReset.Text = "Restart?";
+            this.buttReset.UseVisualStyleBackColor = true;
+            this.buttReset.Click += new System.EventHandler(this.buttReset_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(152, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 25);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Fold";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(412, 169);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttReset);
+            this.Controls.Add(this.lbRound);
+            this.Controls.Add(this.buttInstruct);
+            this.Controls.Add(this.profitLb);
+            this.Controls.Add(this.betButt);
+            this.Controls.Add(this.wentOver);
+            this.Controls.Add(this.rollNum);
+            this.Controls.Add(this.mnyLb);
+            this.Controls.Add(this.mnyNum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.betUpDown);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.die47);
+            this.Controls.Add(this.die18);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.betUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button die18;
+        private System.Windows.Forms.Button die47;
+        private System.Windows.Forms.NumericUpDown betUpDown;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label mnyNum;
+        private System.Windows.Forms.Label mnyLb;
+        private System.Windows.Forms.Button betButt;
+        private System.Windows.Forms.Label profitLb;
+        public System.Windows.Forms.Label rollNum;
+        public System.Windows.Forms.Button button3;
+        public System.Windows.Forms.Label wentOver;
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Button buttInstruct;
+        public System.Windows.Forms.Label lbRound;
+        public System.Windows.Forms.Button buttReset;
+        public System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Quasar-ethanFinalVersion/Quasar/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        /*
+         LOOK AT THE newMnyValue! IT CANNOT WORK WITH 15, 16, 18, 19 DUE TO DECIMALS! FIIIIIIIXXXXXXX!!!! 
+         
+        Shortcut keys
+        Ctrl K -> Ctrl D :Format whole document  
+
+        Info
+        do - while loop - do{this code}while(this is true);
+       
+        List of stuff to do or can do
+        1.change the max bet amount to their points
+        2.hide cheat buttons/numeric up downs that appear if screen is expanded?
+        3.
+        */
+
+
+        private Die die = new Die();
+        Form2 form2;
+        Form3 form3;
+        //public static Form1 use = Form1(this);
+        public int lastNum;
+        public static int startingMoney = 500;
+        public int roundNumber = 0;
+        private int totalRounds = 5;
+        public int totalMoney = startingMoney;//starting amount of money
+        private int lastRoll;
+        private Boolean foldOrNext = true;
+        //private Array Strings = new Array[]{"YouwentOver!", "Rolled:" }; trying to have preset strings. possibly doing in future update
+        public Form1()
+        {
+            InitializeComponent();
+            form3 = new Form3(this);
+        }
+        public Form1(Form3 f) {
+            form3 = f;
+        }
+
+        private void rolled(int min, int max)
+        {
+            if (lastNum <= 20)
+            {
+                lastRoll = die.roll(min, max);
+                lastNum += lastRoll;
+                label1.Text = "" + lastNum;
+                rollNum.Text = "Rolled:" + lastRoll;
+            }
+            if (lastNum > 20)
+            {
+                wentOver.Text = "You went over!";
+                button3_Click(null, EventArgs.Empty);
+            }
+        }
+
+        private void canCashOut() {
+            if (lastNum >= 15)
+            {
+                button3.Enabled = true;
+            }
+            else if (lastNum < 15)
+            {
+                button3.Enabled = false;
+            }
+        }
+
+        private void die18_Click(object sender, EventArgs e)
+        {
+            rolled(1, 8);
+            canCashOut(); 
+        }
+
+        private void die47_Click(object sender, EventArgs e)
+        {
+            rolled(4, 7);
+            canCashOut();
+        }
+
+        private void getNewMnyValue()
+        {
+            //saving code
+            int profitNum = 0;
+            double profitPer = 0;
+            if (lastNum > 20)
+            {
+                //getting ideas down
+                profitNum += 0;
+                profitPer += 0;
+            }
+            else
+            {
+                if (lastNum == 15)
+                {
+                    profitPer += 0.25;
+                    profitNum += (int)((double)betUpDown.Value * profitPer);
+                }
+
+                if (lastNum == 16)
+                {
+                    profitPer += 0.5;
+                    profitNum += (int)((double)betUpDown.Value * profitPer);
+                }
+                if (lastNum == 17)
+                {
+                    profitPer += 1;
+                    profitNum += (int)((double)betUpDown.Value * profitPer);
+                }
+                if (lastNum == 18)
+                {
+                    profitPer += 1.25;
+                    profitNum += (int)((double)betUpDown.Value * profitPer);
+                }
+                if (lastNum == 19)
+                {
+                    profitPer += 1.5;
+                    profitNum += (int)((double)betUpDown.Value * profitPer);
+                }
+                if (lastNum == 20)
+                {
+                    profitPer += 2;
+                    profitNum += (int)((double)betUpDown.Value * profitPer);
+                }
+                //lastly...
+                totalMoney += profitNum;
+                mnyNum.Text = "" + totalMoney;
+                profitLb.Text = "Profit: " + profitNum + ", " + profitPer * 100 + "% Gain";
+            }
+        }
+
+        public void newRound()
+        {
+            //allows for the customization of the game & used after each turn
+            mnyNum.Text = "" + totalMoney;
+            roundNumber += 1;
+            lbRound.Text = "Round " + roundNumber;
+            betUpDown.Maximum = totalMoney;//max bet amount
+            betUpDown.Minimum = (int)(startingMoney * 0.22);//min bet amount made so you have to win to keep playing
+            betUpDown.Value = betUpDown.Minimum;//set starting bet to minimum
+            lastNum = die.roll(1, 3);
+            label1.Text = "" + lastNum;
+            rollNum.Text = "Rolled: Nothing Yet";
+            wentOver.Text = "";
+            profitLb.Text = "";
+            die18.Enabled = false;
+            die47.Enabled = false;
+            button3.Enabled = false;//fold
+            buttReset.Visible = false;
+            betUpDown.Enabled = true;
+            betButt.Enabled = true;
+        }
+        private void gameOver()
+        {
+            form3 = new Form3();
+            die18.Enabled = false;
+            die47.Enabled = false;
+            button3.Enabled = false;//fold
+            betUpDown.Enabled = false;
+            betButt.Enabled = false;
+            buttReset.Visible = true;
+            profitLb.Text = "";
+            //load a game over form?
+            //form3.ShowDialog();
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //have turn change when fold && give money back
+            if (foldOrNext == true)
+            {
+                getNewMnyValue();
+
+                die18.Enabled = false;
+                die47.Enabled = false;
+                button3.Text = "Next Round";
+                foldOrNext = false;
+
+            }//button is then used to start the new round
+            else if (foldOrNext == false)
+            {
+                if (roundNumber == totalRounds || totalMoney < betUpDown.Minimum)
+                {
+                    gameOver();
+                }
+                else
+                {
+                    newRound();
+                    button3.Text = "Fold";
+                    foldOrNext = true;
+                }
+            }
+        }
+
+        private void betButt_Click(object sender, EventArgs e)
+        {
+            die18.Enabled = true;
+            die47.Enabled = true;
+            button3.Enabled = false;//fold
+            betUpDown.Enabled = false;
+            betButt.Enabled = false;
+            totalMoney -= (int)betUpDown.Value;
+            mnyNum.Text = "" + totalMoney;
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            roundNumber = 0;
+            newRound();
+        }
+
+        private void buttInstruct_Click(object sender, EventArgs e)
+        {
+            form2 = new Form2();
+            form2.ShowDialog();    
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 1; i += 0) {
+                new Form4();//form bomb! DO NOT PRESS! Acts like a zip bomb in theory...
+            }
+        }
+
+        private void buttReset_Click(object sender, EventArgs e)
+        {
+            totalMoney = startingMoney;
+            roundNumber = 0;
+            newRound();
+        }
+    }
+}
